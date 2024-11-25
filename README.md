1ab79799726e2b01dfb8f3b41834b8b59e86a9e3
Juan Álvaro Flórez <jflorc01@estudiantes.unileon.es>
2024-11-25 18:16:44 +0100

    Añadido movimiento

--- README.md
+++ README.md
@@ -7,6 +7,19 @@
     * 3: Meta.
 
 *Funcionamiento:* 
-    * Es una cuadrícula.
-    * Se generan dinámicamente los divs con la clase "muro" o "camino" en función del número del array
-    * Se registran las teclas de las flechas para moverse.
+    * Canvas que imprime una cuadrícula del tamaño del array del laberinto.
+    * Casill negra: Muros.
+    * Casilla blanca: Camino.
+    * Casilla verde: Posición del jugador.
+    * Casilla roja: meta,
+
+*Tareas:*
+    [x] Imprimir laberinto.
+    [x] Mover posición.
+    [x] Impedir que se mueva en los muros.
+    [ ] Imprimir solamente las casillas de alrededor del jugador.
+
+*Ideas:*
+    * La casilla vibra más cuanto más cerca de la meta.
+    * Elemento que se mueva aleatoriamente por los caminos y que si te alcanza pierdes.
+    * Editor de mapas. Dibujar una cuadrícula y en las casillas en las que se haga clic serán muros. (Posible animación al añadir el muro).
--- js/script.js
+++ js/script.js
@@ -1,13 +1,90 @@
-const laberinto = [
+let laberinto = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     [1, 2, 0, 0, 1, 0, 0, 0, 0, 1],
     [1, 0, 1, 0, 1, 0, 1, 1, 0, 1],
     [1, 0, 1, 0, 0, 0, 1, 0, 0, 1],
     [1, 0, 1, 1, 1, 0, 1, 1, 0, 1],
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+    [1, 1, 1, 0, 1, 1, 1, 1, 0, 1],
     [1, 0, 0, 0, 0, 0, 0, 1, 0, 1],
     [1, 0, 1, 1, 1, 1, 0, 1, 3, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 ];
+
+const canvas = document.getElementById('laberinto');
+    const ctx = canvas.getContext('2d');
+    const tileSize = 50;
+
+    function dibujarLaberinto(matriz) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        for (let fila = 0; fila < matriz.length; fila++) {
+            for (let columna = 0; columna < matriz[fila].length; columna++) {
+                const valor = matriz[fila][columna];
+                const x = columna * tileSize;
+                const y = fila * tileSize;
+
+                if (valor === 1) {
+                    ctx.fillStyle = 'black';
+                } else if (valor === 2) {
+                    ctx.fillStyle = 'green';
+                } else if (valor === 3) {
+                    ctx.fillStyle = 'red';
+                } else {
+                    ctx.fillStyle = 'white';
+                }
+
+                ctx.fillRect(x, y, tileSize, tileSize);
+                ctx.strokeStyle = 'gray';
+                ctx.strokeRect(x, y, tileSize, tileSize);
+            }
+        }
+    }
+
+    function mover(matriz, x, y) {
+        let mat = matriz;
+        let posx, posy;
+
+        for (let i = 0; i < mat.length; i++) {
+            for (let j = 0; j < mat[i].length; j++) {
+                if (mat[i][j] === 2) {
+                    posx = i;
+                    posy = j;
+                }
+            }
+        }
+
+        if (
+            mat[posx + x][posy + y] !== undefined &&
+            mat[posx + x][posy + y] !== 1
+        ) {
+            mat[posx][posy] = 0;
+            mat[posx + x][posy + y] = 2;
+        }
+
+        return mat;
+    }
+
+    dibujarLaberinto(laberinto);
+
+    document.addEventListener('keydown', (event) => {
+        let x = 0, y = 0;
+
+        if (event.key === 'ArrowUp') {
+            event.preventDefault();
+            x = -1;
+        } else if (event.key === 'ArrowDown') {
+            event.preventDefault();
+            x = 1;
+        } else if (event.key === 'ArrowLeft') {
+            event.preventDefault();
+            y = -1;
+        } else if (event.key === 'ArrowRight') {
+            event.preventDefault();
+            y = 1;
+        }
+
+        laberinto = mover(laberinto, x, y);
+        dibujarLaberinto(laberinto);
+    });
 
